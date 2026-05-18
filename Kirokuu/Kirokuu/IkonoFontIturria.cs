dbecc963e7b4d2cca0fce6260fbd65350fa248ee
Eneko Galan <enekogalanelorza@gmail.com>
2026-05-18 19:58:30 +0200

    merge

--- Kirokuu/Kirokuu/IkonoFontIturria.cs
+++ Kirokuu/Kirokuu/IkonoFontIturria.cs
@@ -21,8 +21,6 @@ public static class IkonoFontIturria
 
     public static FontImageSource FitxaNireTxartelak(double tamaina = 26) => Sortu("\uef6e", tamaina);
 
-    public static FontImageSource FitxaTxartelKanban(double tamaina = 26) => Sortu("\ueb7f", tamaina);
-
     public static FontImageSource FitxaHasieraAdministratzaile(double tamaina = 26) => Sortu("\ue871", tamaina);
 
     public static FontImageSource FitxaErabiltzaileZerrenda(double tamaina = 26) => Sortu("\ue7ef", tamaina);
